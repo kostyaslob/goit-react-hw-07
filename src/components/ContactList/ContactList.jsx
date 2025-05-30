2e8ce9393cab807c0226f0884922f5ead0eea844
kostyaslob <kostyaslob@gmail.com>
2025-05-31 00:42:00 +0300

    add createSelector for selectFilteredContacts

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,19 +1,15 @@
 import css from "./ContactList.module.css"
 
 import { Contact } from "../Contact/Contact";
-import { selectContacts } from "../../redux/contactsSlice.js"
-import { selectNameFilter } from "../../redux/filtersSlice.js";
-
+import { selectFilteredContacts } from "../../redux/contactsSlice.js"
 import { useSelector } from "react-redux";
 
 export const ContactList = () => {
-    const contacts = useSelector(selectContacts);
-    const filters = useSelector(selectNameFilter);
-    const visibleContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filters.toLowerCase()));
+    const contacts = useSelector(selectFilteredContacts)
 
     return (
         <ul className={css.contactList}>
-            {visibleContacts.map((contact) => (
+            {contacts.map((contact) => (
                 <li className={css.contactItem} key={contact.id}>
                     <Contact contact={contact}  />
                 </li>
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,5 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createSelector } from "@reduxjs/toolkit";
 import { fetchContacts, deleteContact, addContact } from "./contactsOps";
+import { selectNameFilter } from "./filtersSlice";
 
 const slice = createSlice({
     name: "contacts",
@@ -41,4 +42,8 @@ export default slice.reducer;
 
 export const selectContacts = (state) => state.contacts.items;
 export const selectLoading = (state) => state.contacts.loading;
-export const selectError = (state) => state.contacts.error;  
+export const selectError = (state) => state.contacts.error; 
+
+export const selectFilteredContacts = createSelector([selectContacts, selectNameFilter], (contacts, filters) => {
+    return contacts.filter(contact => contact.name.toLowerCase().includes(filters.toLowerCase()));
+})
