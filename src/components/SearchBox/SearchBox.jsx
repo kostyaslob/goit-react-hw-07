c143123549a401704b173287196650d90cfb93a3
kostyaslob <kostyaslob@gmail.com>
2025-05-31 01:06:49 +0300

    fix id in SearchBox

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -2,9 +2,11 @@ import css from "./SearchBox.module.css";
 import { FaSearch } from "react-icons/fa";
 
 import { useSelector, useDispatch } from "react-redux";
+import { useId } from "react";
 import { changeFilter, selectNameFilter } from "../../redux/filtersSlice.js";
 
 export const SearchBox = () => {
+    const id = useId()
     const dispatch = useDispatch();
     const search = useSelector(selectNameFilter);
   
@@ -18,6 +20,7 @@ export const SearchBox = () => {
             <input
                 className={css.searchInput}
                 type="text"
+                id={id}
                 value={search}
                 onChange={handleChange}
             />
