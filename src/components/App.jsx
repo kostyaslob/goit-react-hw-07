5148667518e0673316c9fe039bf76787a3279579
kostyaslob <kostyaslob@gmail.com>
2025-05-30 18:10:14 +0300

    add ErrorMessage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,15 @@ import { ContactForm } from "./ContactForm/ContactForm";
 import { SearchBox } from "./SearchBox/SearchBox";
 import { ContactList } from "./ContactList/ContactList";
 import { Loader } from "./Loader/Loader";
+import {ErrorMessage} from "./ErrorMessage/ErrorMessage"
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchContacts } from "../redux/contactsOps";
 
 export default function App() { 
   const dispatch = useDispatch();
-  const loading = useSelector(state => state.contacts.loading);
+  const loading = useSelector((state) => state.contacts.loading);
+  const error = useSelector((state) => state.contacts.error);
 
   useEffect(() => {
     dispatch(fetchContacts())
@@ -21,7 +23,8 @@ export default function App() {
       <h1>Phonebook</h1>
       <ContactForm  />
       <SearchBox />
-      {loading && <Loader/> }
+      {loading && <Loader />}
+      {error && <ErrorMessage/>}
       <ContactList />
     </div>
   );
